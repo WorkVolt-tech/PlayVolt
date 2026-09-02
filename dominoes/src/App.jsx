18d8cb244a3ddd37612dbf4e0e82e76dde77f5e2
WorkVolt-tech <ancmarx@gmail.com>
2026-09-01 23:05:20 -0400

    Wrap routes in DragProvider for drag and drop support

--- dominoes/src/App.jsx
+++ dominoes/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { DragProvider } from './components/DragDrop'
 import Lobby from './pages/Lobby'
 import Game from './pages/Game'
 import './styles/global.css'
@@ -6,11 +7,13 @@ import './styles/global.css'
 export default function App() {
   return (
     <BrowserRouter>
+      <DragProvider>
       <Routes>
         <Route path="/" element={<Lobby />} />
         <Route path="/game" element={<Game />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+      </DragProvider>
     </BrowserRouter>
   )
 }
