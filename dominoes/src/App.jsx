c769da8aadc5c9a3806082049b76dc5fed23465e
WorkVolt-tech <ancmarx@gmail.com>
2026-09-02 23:52:44 -0400

    Add Tracker route to App component

--- dominoes/src/App.jsx
+++ dominoes/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { DragProvider } from './components/DragDrop'
 import Lobby from './pages/Lobby'
+import Tracker from './pages/Tracker'
 import Game from './pages/Game'
 import './styles/global.css'
 
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Lobby />} />
         <Route path="/game" element={<Game />} />
+        <Route path="/tracker" element={<Tracker />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       </DragProvider>
